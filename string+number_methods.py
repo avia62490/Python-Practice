5bc674dc86fc836114ff593832c8ad40b8d0680b
Alex Via <alexandervia@gmail.com>
2023-12-20 14:31:28 -0600

    completes exercise

---
+++ string+number_methods.py
@@ -0,0 +1,60 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+# word = input("Enter a word: ")
+# print(word.upper())
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# number = int(input("Enter a number: "))
+# if number>100:
+#   print("That's a big number")
+# else:
+#   print("That's not too big")
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+# print("The total is: " + str(num1 + num2))
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# word = input("Enter a word: ")
+# print(word[::-1])
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# number = input("enter a number: ")
+# print(number * 10)
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# word1 = input("Enter a word: ")
+# word2 = input("Enter another word: ")
+# print(word1.upper() + " " + word2.upper())
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# word = input("enter a word: ")
+# print("that word is %d letters long."%(len(word)))
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# number = int(input("enter a number: "))
+# if number<0:
+#   print("that's a negative number.")
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# num1 = int(input("enter a number: "))
+# num2 = int(input("enter another number: "))
+# print("the product of %d and %d  is %d"%(num1, num2, num1 * num2))
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+# word = input("enter a word: ")
+# if len(word)>5:
+#   print('thats a long word')
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
