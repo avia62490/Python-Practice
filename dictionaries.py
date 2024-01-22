1bc06a39643f24884bf3925c173304bf9b506904
Alex Via <alexandervia@gmail.com>
2024-01-22 14:02:16 -0600

    solves exercises

---
+++ dictionaries.py
@@ -0,0 +1,79 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+# contact = {
+#   "first_name": "Alex",
+#   "last_name": "Via",
+#   "email": "alex@via.com"
+# }
+
+# for x in contact.values():
+#   print(x)
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+# contacts = [
+#   {
+#     "first_name": "Homer",
+#     "last_name": "Simpson"
+#   },
+#   {
+#     "first_name": "Darth",
+#     "last_name": "Vader"
+#   },
+#   {
+#     "first_name": "John",
+#     "last_name": "Doe"
+#   }
+# ]
+
+# for x in contacts[0].values():
+#   print(x)
+
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+# menu = {
+#   "pizza": 5,
+#   "fries": 3,
+#   "nuggets": 4
+# }
+# for x, y in menu.items():
+#   print(x, y)
+
+# menu["shake"] = 4
+# for x, y in menu.items():
+#   print(x, y)
+
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+# book = {
+#   "title": "How to Change Your Mind",
+#   "author": "Michael Pollan",
+#   "pages": 350,
+#   "language": "english"
+# }
+
+# for x, y in book.items():
+#   print(x, y)
+
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+# books = [
+#   {"title": "bible", "author": "jesus"},
+#   {"title": "ooga booga", "author": "mr clean"},
+#   {"title": "philosphy", "author": "plato"}
+# ]
+
+# print(books[2]["author"])
+
+# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/d257aec07882d78009bd796ed53f81bb
