c6e35ad121aa2044cae055c151909855c18e4be5
Alex Via <alexandervia@gmail.com>
2024-01-24 12:26:44 -0600

    solves exercises

--- functions.py
+++ functions.py
@@ -35,13 +35,40 @@
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def we_three_strings(x, y, z):
+#   return x + " " + y + " " + z 
+
+# print(we_three_strings("this", "function", "works"))
+
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# def stringify(num):
+#   return str(num)
+
+# x = stringify(55)
+# print(stringify(55))
+# print(type(x))
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
+# def five_times(string):
+#   return string * 5
+
+# print(five_times("hello"))
+
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def average(x, y, z):
+#   return (x + y + z) / 3
+
+# print(average(5, 5, 6))
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+# def doohicky(x):
+#   return x * 10 + 30
+
+# print(doohicky(4))
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
