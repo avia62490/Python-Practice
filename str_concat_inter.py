bda6d794e859f1552dc4a4ad76e4fed55acd4cb4
Alex Via <alexandervia@gmail.com>
2024-03-03 13:22:14 -0600

    sharpens skills

--- str_concat_inter.py
+++ str_concat_inter.py
@@ -23,17 +23,40 @@ color3 = "yellow"
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
-print("the primary colors are %s, %s, and %s"%(color1, color3, color2))
+# print("the primary colors are %s, %s, and %s"%(color1, color3, color2))
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+# name = input("WHat's your name? ")
+# if name == "Santa":
+#   print("I'm Bart Simpson, who the hell are you?")
+# else:
+#   print("You're not Santa")
+
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+title = "Sapiens"
+author = "Yuval NH"
+# print(author + " is the author of the book '" + title +"'")
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+# print("I read the book titled '%s' and it was written by %s"%(title, author))
+
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+# password = input("whats the passsword? ")
+# if password == "Joshua":
+#   print("Shall we play a game?")
+# else:
+#   print("Access denied")
+
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+city1 = "Chicago"
+city2 = "Seattle"
+city3 = "Los Angeles"
+print("I have family in " + city1 +", " + city2 + ", and " + city3)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
