13e2272293cf491458ef5ff24984f6fab08383c1
Alex Via <alexandervia@gmail.com>
2024-03-03 14:07:44 -0600

    sharpens skills

--- string+number_methods.py
+++ string+number_methods.py
@@ -27,13 +27,30 @@
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
+# word1 = input("enter a word: ")
+# word2 = input("enter another word: ")
+# print(word1.upper() + " " + word2.upper())
+
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# word = input("Enter a word: ")
+# print("%s has %i letters in it"%(word, len(word)))
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+# number = int(input("Enter a number: "))
+# if number < 0: print("Thats a negative number")
+
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+# print("The product of %i and %i is %i"%(num1, num2, num1 * num2))
+
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+word = input("Enter a word: ")
+if len(word) > 5: print("thats a big word")
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
