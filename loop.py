5ed63780a21e0a5ff252eabea54ff7d3f32e3efb
Alex Via <alexandervia@gmail.com>
2024-03-12 19:07:27 -0500

    sharpens skills

--- loop.py
+++ loop.py
@@ -1,66 +1,41 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
 # for x in range(1, 11):
-#     print(x)
+#   print(x)
 
 # 2. Write a while loop that prints the word "hello" 5 times.
 
-# count = 0
-# while count < 5:
-#     print("hello %d" % (count))
-#     count += 1
+# for x in range(1, 6):
+#   print("hello")
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
 # while True:
-#     word = input("enter a word: ")
-#     if word == "stop":
-#         break
+#   word = input("Enter a word: ")
+#   if word == "stop":
+#     break
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
-# for num in range(0, 101, 5):
-#     print(num)
+# count = 0
+# while count < 101:
+#   print(count)
+#   count += 5
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
-# index = 0
-# while index < 10:
-#     print(9000)
-#     index += 1
+# for x in range(1, 11):
+#   print(9000)
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
-# while True:
-#     num = int(input("enter a number: "))
-#     if num > 10:
-#         break
-#     else:
-#         print("not big enough")
-
 # 7. Write a while loop that prints the numbers 50 to 70.
-
-# for x in range(50, 55):
-#     print("Hello %d" % (x))
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
-# for x in range(0, 144):
-#     print("Around the world %d" % (x))
-
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
-# while True:
-#     word = input("enter a word: ")
-#     if len(word) > 5:
-#         break
-#     else:
-#         print("write a bigger word")
-
 # 10. Write a while loop that prints the even numbers from 2 to 40.
-
-# for num in range(2, 41, 2):
-#     print(num)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
