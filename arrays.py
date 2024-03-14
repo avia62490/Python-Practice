57fb7ef1674938134dbbf46884bbaa23ef9de670
Alex Via <alexandervia@gmail.com>
2024-03-14 12:24:49 -0500

    sharpens skills

--- arrays.py
+++ arrays.py
@@ -30,13 +30,33 @@
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
+# names = ['Alex', 'Chase', 'Eben']
+# for name in names:
+#   print(name)
+
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# strings = ['boat', 'table']
+# strings.append('road')
+# print(strings)
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+# nums = [1, 2, 3, 4, 5]
+# nums[0] *= 10
+# print(nums)
+
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
+# nums = [69, 420]
+# for x in nums:
+#   print(x)
+
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# countries = ["USA", "Japan", "Finland"]
+# countries.append("India")
+# print(countries)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
