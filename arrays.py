31ce4f35113143041e03ab54c0fdba8f734fc7c8
Alex Via <alexandervia@gmail.com>
2024-03-14 12:18:59 -0500

    sharpens skills

--- arrays.py
+++ arrays.py
@@ -1,63 +1,42 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-# list = ["rain", "sunny", "snow"]
-# print(list)
-# list.extend(("sleet", "drizzle"))
+# list = ['eenie', 'meenie', 'minie']
+# list.extend(('mo', 'tiger'))
 # print(list)
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-# letters = ["a", "b", 'c', 'd']
-# letters[1] = 2
-# print(letters)
+# characters = ['a', 'b', 'c', 'd']
+# characters[1] = 2
+# print(characters)
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-# nums = [5, 8, 13, 21, 34]
+# nums = [1, 2, 3, 4, 5]
 # for x in nums:
-#     print(x)
+#   print(x)
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
-# nums = [5]
-# nums.extend((11, 23, 44))
+# nums = [3]
+# nums.extend((9, 27, 81))
 # print(nums)
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
-# words = ['table', 'couch', 'television']
+# words = ['banana', 'tattoo', 'fireplace']
 # words[2] = words[2].upper()
 # print(words)
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
-# names = ['alex', 'chase', 'eben']
-# for x in names:
-#   print(x)
-
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
-
-# strings = ['one', 'two']
-# strings.append('banana')
-# print(strings)
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
-# nums = [3, 6, 12, 19, 25]
-# nums[0] = nums[0] * 10
-# print(nums)
-
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
-# nums = [3, 8]
-# for x in nums:
-#   print(x)
-
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
-
-# countries = ['america', 'canada', 'japan']
-# countries.append('australia')
-# print(countries)
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
