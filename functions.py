45d431b12cb464751345f0f35e98ac16554ca826
Alex Via <alexandervia@gmail.com>
2024-03-26 12:33:33 -0500

    sharpens skills

--- functions.py
+++ functions.py
@@ -28,20 +28,46 @@
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-def first_letter(string):
-  return string[0]
+# def first_letter(string):
+#   return string[0]
 
-print(first_letter("hello"))
+# print(first_letter("hello"))
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def combine(x, y, z):
+#   return x + " " + y + " " + z
+
+# print(combine("see", "spot", "run"))
+
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# def stringify(num):
+#   return str(num)
+
+# print(stringify(6))
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
+# def repeat(string):
+#   return string * 5
+
+# print(repeat("hello"))
+
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def average(num1, num2, num3):
+#   return (num1 + num2 + num3) / 3
+
+# print(average(2, 2, 3))
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+# def mathmatical(num):
+#   return num * 10 + 30
+
+# print(mathmatical(4))
+# print(mathmatical(8))
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
