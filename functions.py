72ac79f9a47ec9c4f440acb61bcf229e1fd038cc
Alex Via <alexandervia@gmail.com>
2024-03-26 12:22:55 -0500

    sharpens skills

--- functions.py
+++ functions.py
@@ -1,74 +1,47 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-# def times_two(x):
-#   return x * 2
+# def times_two(num):
+#   return num * 2
 
 # print(times_two(4))
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
-# def all_caps(string):
+# def capitalize(string):
 #   return string.upper()
 
-# print(all_caps("hello"))
+# print(capitalize("hello"))
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
-# def subtract(x, y):
-#   return x - y
+# def subtraction(num1, num2):
+#   return num1 - num2
 
-# print(subtract(10, 3))
+# print(subtraction(8, 3))
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
-# def squared(x):
-#   return x ** 2
+# def squared(num):
+#   return num ** 2
 
-# print(squared(5))
+# print(squared(4))
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-# def first_letter(string):
-#   return string[0]
+def first_letter(string):
+  return string[0]
 
-# print(first_letter("hello"))
+print(first_letter("hello"))
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
-# def we_three_strings(x, y, z):
-#   return x + " " + y + " " + z 
-
-# print(we_three_strings("this", "function", "works"))
-
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
-
-# def stringify(num):
-#   return str(num)
-
-# x = stringify(55)
-# print(stringify(55))
-# print(type(x))
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
-# def five_times(string):
-#   return string * 5
-
-# print(five_times("hello"))
-
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
-# def average(x, y, z):
-#   return (x + y + z) / 3
-
-# print(average(5, 5, 6))
-
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
-
-# def doohicky(x):
-#   return x * 10 + 30
-
-# print(doohicky(4))
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
